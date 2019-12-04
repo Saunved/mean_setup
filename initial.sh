e5eb4411ae3181659da7abffb5ade41adb1bdc13
Saunved <saunved@gmail.com>
2019-12-04 16:12:59 +0530

    Added feedback notes after each command

--- initial.sh
+++ initial.sh
@@ -3,7 +3,11 @@
 # Login with ssh root@<your-ip>
 # Run ./initial.sh <user-name>
 adduser $1
+echo "Added user $1"
 usermod -aG sudo $1
+echo "Gave sudo permissions to $1"
 ufw allow OpenSSH
 ufw enable
+echo "Enabled OpenSSH on the firewall"
 rsync --archive --chown=$1:$1 ~/.ssh /home/$1
+echo "Added root public ssh key to user $1"
